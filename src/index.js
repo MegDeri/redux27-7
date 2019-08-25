867ed2769484351f643b9f16902832c6bc6cf8f2
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-08-25 23:24:26 +0300

    Change index.js & Reducer.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
